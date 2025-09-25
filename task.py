385ec70d5ff7d23deee0e7ea08be5084b6afed67
ASTRO-ANIKET <aniketshrivastava12345@gmail.com>
2025-09-26 01:52:33 +0530

    Add files via upload

---
+++ task.py
@@ -0,0 +1,4 @@
+print("Welcome to the band name Generator")
+var1 = input("What's the name of the city you grew up in?\n")
+var2 = input("What's your pet's name?\n")
+print("Your band name could be: " + var1 +" " + var2)
